3c7f4206c872fe67084c899591f6771b863a5d13
notGabo <ga.soto@duocuc.cl>
2025-06-17 16:54:27 -0400

    feature borrar

--- args.sh
+++ args.sh
@@ -5,7 +5,9 @@ VERSION="0.1"
 DATE=$(date +%Y%m%d)
 
 BORRAR=false
+condiciones_logfile=""
 
+# Control de argumentos
 for arg in "$@"
 do
   case $arg in
@@ -31,7 +33,7 @@ do
     --dias=*)
         DIAS="${arg#*=}"
         ;;
-    --borrar=*)
+    --borrar)
         BORRAR=true
         ;;
     --logfile=*)
@@ -48,15 +50,23 @@ do
   esac
 done
 
+# Validación de argumentos
 if [ -z "$ARCHIVOS" ] || [ -z "$SALIDA" ] || [ -z "$ENTRADA" ] || [ -z "$DIAS" ]; then
   echo "Error: Los argumentos --archivos, --salida, --entrada y --dias son obligatorios."
   exit 1
 fi
-
-# Comando find para obtener los archivos
-condiciones_archivos="$(echo "$ARCHIVOS" | sed 's/,/" -o -name "*./g' | sed 's/^/-name "*./' | sed 's/$/"/')"
-condiciones_logfile=""
+# Validación de logfile
 if [ -n "$LOGFILE" ]; then
   condiciones_logfile=">> \"$LOGFILE\" 2>&1"
 fi
+
+# Construcción de condiciones para find
+condiciones_archivos="$(echo "$ARCHIVOS" | sed 's/,/" -o -name "*./g' | sed 's/^/-name "*./' | sed 's/$/"/')"
+
+# Ejecucion del script
 eval "find \"$ENTRADA\" -type f -mtime -\"$DIAS\" \\( $condiciones_archivos \\) -print | zip -r \"$SALIDA/backup_files_hasta_${DATE}.zip\" -@ $condiciones_logfile"
+
+# Borrado de archivos si se especifica
+if [ "$BORRAR" = true ]; then
+    eval "find \"$ENTRADA\" -type f -mtime -\"$DIAS\" \\( $condiciones_archivos \\) -print0 | xargs -0 rm -f >> \"$LOGFILE\" 2>&1"
+fi
